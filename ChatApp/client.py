94a4a104cc463df30d1fd0a1e2722e6711723d38
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-03-29 19:01:31 +0200

    chatApp finished

---
+++ ChatApp/client.py
@@ -0,0 +1,73 @@
+import socket
+import threading
+import json
+
+HOST = '127.0.0.1'
+PORT = 5050
+
+
+def listen_server(client):
+    while True:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            message = json.loads(message)
+            print(f"{message['sender']}: {message['message']} ({message['timestamp']})")
+        else:
+            print('Received empty message.')
+
+
+def send_message(client):
+    while True:
+        message = input()
+        if message != '':
+            client.sendall(message.encode('utf-8'))
+        else:
+            print('Message cannot be empty. Please try again.')
+
+
+def send_to_server(client):
+    username = input('Enter your username: ')
+    if username != '':
+        action = input('Enter 1 to chat with a user, 2 to chat with a channel: ')
+        if action == '1':
+            username = username + ';' + action
+            client.sendall(username.encode('utf-8'))
+            select_user_to_chat(client)
+        elif action == '2':
+            username = username + ';' + action
+            client.sendall(username.encode('utf-8'))
+        else:
+            print('Invalid action. Please try again.')
+            exit(0)
+    else:
+        print('Username cannot be empty. Please try again.')
+        exit(0)
+    
+    threading.Thread(target=listen_server, args=(client, )).start()
+    send_message(client)
+
+
+def select_user_to_chat(client):
+    receiver = input('Enter the username you want to chat with: ')
+    if receiver != '':
+        client.sendall(receiver.encode('utf-8'))
+    else:
+        print('Receiver cannot be empty. Please try again.')
+        exit(0)
+
+
+def main():
+    # Client side socket creation for TCP connection.
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((HOST, PORT))
+        print(f'Connected to {HOST}:{PORT}')
+    except socket.error as e:
+        print('Error occured while trying to connect: ' + str(e))
+
+    send_to_server(client)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ChatApp/server.py
@@ -0,0 +1,129 @@
+import datetime
+import socket
+import threading
+import json
+
+HOST = '127.0.0.1'
+PORT = 5050
+SERVER_LIMIT = 8
+
+active_users = []
+sessions = []
+
+def listen_messages_all(client, username):
+    while True:
+        try:
+            message = client.recv(2048).decode('utf-8')
+            if message != '':
+                msg_object = {
+                    'sender': username + '[GLOBAL]',
+                    'message': message,
+                    'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+                print(msg_object)
+                send_message_to_all(json.dumps(msg_object))
+            else:
+                print('Message cannot be empty. Please try again.')
+        except:
+            print('user disconnected.')
+            break
+
+
+def listen_messages_private(client, username, receiver):
+    while True:
+        try:
+            message = client.recv(2048).decode('utf-8')
+            if message != '':
+                msg_object = {
+                    'sender': username + '[PRIVATE]',
+                    'message': message,
+                    'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+                print(msg_object)
+                send_message_to_user(receiver, json.dumps(msg_object))
+                send_message_to_user(client, json.dumps(msg_object))
+            else:
+                print('Message cannot be empty. Please try again.')
+        except:
+            print('user disconnected.')
+            break
+
+
+def send_message_to_user(client, message):
+    client.sendall(message.encode('utf-8'))
+
+
+def send_message_to_all(message):
+    for user in active_users:
+        send_message_to_user(user[1], message)
+
+
+def handle_client(client):
+    while True:
+        response = client.recv(2048).decode('utf-8')
+        if response != '':
+            response = response.split(';')
+
+            username = response[0]
+            selection = response[1]
+            active_users.append((username, client))
+            if selection == '1':
+                private_chat(client, username)
+                break
+            elif selection == '2':
+                new_user_message = {
+                    'sender': 'SERVER',
+                    'message': f'{username} has joined to the global chat.',
+                    'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+                send_message_to_all(json.dumps(new_user_message))
+                threading.Thread(target=listen_messages_all, args=(client, username)).start()
+                break
+        else:
+            print('Username cannot be empty. Please try again.')
+
+
+def private_chat(client, username):
+    while True:
+        receiver = client.recv(2048).decode('utf-8')
+        for user in active_users:
+            if (user[0] == receiver):
+                client_2 = user[1] 
+                sessions.append(((username, client), (receiver, client_2)))
+                server_msg = {
+                    'sender': 'SERVER',
+                    'message': f'Private chat with {receiver} created.',
+                    'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                }
+                client.sendall(json.dumps(server_msg).encode('utf-8'))
+                break
+        break
+    threading.Thread(target=listen_messages_private, args=(client, username, client_2)).start()
+
+
+def disconnect_user(client):
+    client.close()
+    active_users.remove(client)
+
+
+def main():
+    # Create a server socket. SOCK_STREAM means that it is a TCP socket.
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        server.bind((HOST, PORT))
+        print(f'Server is running on {HOST}:{PORT}')
+    except socket.error as e:
+        print('Error occured while trying to connect: ' + str(e))
+    
+    server.listen(SERVER_LIMIT)
+
+    while True:
+        client, address = server.accept()
+        print(f'Connected to client: {address[0]}:{address[1]}')
+
+        threading.Thread(target=handle_client, args=(client, )).start()
+
+
+if __name__ == '__main__':
+    main()
